d7f24c1551f05dfa0ac7f5b182a997e37524c23f
Ayala3027 <adrianalexisjimenezhernandez@gmail.com>
2023-01-31 21:14:57 -0500

    construccion header y estilos

--- client/src/helper/HeaderJoly.jsx
+++ client/src/helper/HeaderJoly.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from "react";
+import {NavLink} from "react-router-dom";
 
 const HeaderJoly = () => {
   return (
-    <section>
-        HeaderJoly
-    </section>
-  )
-}
+    <header>
+      <nav className="menu">
+        <ul className="list">
+          <li className="inicio"><NavLink to= '/Home-joly' >INICIO</NavLink></li>
+          <li><NavLink to= '/Home-joly' >JOLY</NavLink> 
+            <ul>
+              <li><NavLink to= '/clientes-joly'>Clientes</NavLink> </li>
+              <li><NavLink to= '/productos-joly'>Productos</NavLink></li>
+              <li><NavLink to= '/proveedores-joly'>Proveedores</NavLink></li>
+            </ul>
+          </li>
+          
+          <li><NavLink to= '/Home-SyS' >SYS</NavLink> 
+            <ul>
+              <li><NavLink to= '/citas-sys'>Citas</NavLink> </li>
+              <li><NavLink to= '/productos-sys'>Productos</NavLink></li>
+              <li><NavLink to= '/clientes-sys'>Clientes</NavLink></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
-export default HeaderJoly
+export default HeaderJoly;
