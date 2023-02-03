d5b3c25f8dd49c1e43c0d34496afa539763c797e
Ayala3027 <adrianalexisjimenezhernandez@gmail.com>
2023-02-02 20:28:34 -0500

    creación de componentes, contenido de sys y joly y estilos

---
+++ client/src/components/SYS/Galerysys.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Galerysys = () => {
+  return (
+    <div>Galerysys</div>
+  )
+}
+
+export default Galerysys
--- client/src/layout/Joly/HomeJoly.jsx
+++ client/src/layout/Joly/HomeJoly.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Mainjoly from '../../components/JOLY/Mainjoly'
 import HeaderJoly from '../../helper/HeaderJoly'
 
 const HomeJoly = () => {
   return (
     <section>
         <HeaderJoly />
+        <Mainjoly />
     </section>
   )
 }
